f094f5528b0569bc7d89e3f796c9cf4ff15fd846
yiranshu <shuyiran1425@hotmail.com>
2024-03-10 15:59:12 -0400

    parameters

---
+++ index.js
@@ -0,0 +1,24 @@
+/*function logTwoValues(value1, value2) {
+    console.log(`The two values are ${value1} and ${value2}.`);
+  }
+
+function sayHelloTo(firstName) {
+    console.log(`Hello, ${firstName}!`);
+  }
+
+function sayHelloTo(firstName = "User") {
+    console.log(`Hello, ${firstName}!`);
+  }*/
+
+function introduction(name) {
+    console.log(`Hi, my name is ${name}.`);
+  }
+  
+function introductionWithLanguage(name, language) {
+    console.log(`Hi, my name is ${name} and I am learning ${language}.`);
+  }
+  
+  /*
+function introductionWithLanguageOptional(name = "User", language ="JavaScript") {
+    console.log(`Hi, my name is ${name} and I am learning to program in ${language}.`);
+  }*/
